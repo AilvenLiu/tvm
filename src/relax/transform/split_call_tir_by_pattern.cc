0305b47059ef5f1c56d7640432b900033485f851
spectrometerHBH <bohanhou@andrew.cmu.edu>
2026-02-12 21:43:41 -0500

    rebased

--- src/relax/transform/split_call_tir_by_pattern.cc
+++ src/relax/transform/split_call_tir_by_pattern.cc
@@ -46,6 +46,8 @@ namespace tirx {
 using relax::FCodegen;
 using relax::MatchResult;
 using relax::TIRXattern;
+using s_tir::ExprComparator;
+using s_tir::TensorizeComparator;
 
 /*! \brief helper to match a for stmt to a pattern*/
 class ForMatcher : public TensorizeComparator {
